f66c66fb15bd45be96370defdd5b0df723b96473
TheNeonNew <114394777+TheNeonNew@users.noreply.github.com>
2022-09-26 16:17:43 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,90 @@
+import pygame as pg
+from objects import *
+from time import sleep
+
+screen = pg.display.set_mode((600, 650))
+screen.fill(pg.Color('#36454F'))
+
+snake = Snake()
+block_dirs = {}.fromkeys(['w', 's', 'a', 'd'], True)
+
+score_counter = Score()
+
+borderLine = Line(pg.Color("red"), [(0, 50), (600, 50)], 4)
+
+apple = Apple()
+
+run = True
+clock = pg.time.Clock()
+pg.display.flip()
+
+while run:
+    for ev in pg.event.get():
+        if ev.type == pg.QUIT:
+            run = False
+        elif ev.type == pg.KEYDOWN:
+            # Matching pressed keys with 'W', 'A', 'D', 'S', processing snake's movement
+            match ev.key:
+                case pg.K_w if block_dirs['w']:
+                    snake.move_x = 0
+                    snake.move_y = -1
+                    block_dirs = {'w': True, 's': False, 'a': True, 'd': True}
+                case pg.K_s if block_dirs['s']:
+                    snake.move_x = 0
+                    snake.move_y = 1
+                    block_dirs = {'w': False, 's': True, 'a': True, 'd': True}
+                case pg.K_d if block_dirs['d']:
+                    snake.move_x = 1
+                    snake.move_y = 0
+                    block_dirs = {'w': True, 's': True, 'a': False, 'd': True}
+                case pg.K_a if block_dirs['a']:
+                    snake.move_x = -1
+                    snake.move_y = 0
+                    block_dirs = {'w': True, 's': True, 'a': True, 'd': False}
+    screen.fill(pg.Color('#36454F'))
+    if snake.game_over:
+        gameOver = Text("Times New Roman", 40, f"Game Over! Your score was {score_counter.i}", pg.Color("red"),
+                        (35, 175))
+        restartHint = Text("Times New Roman", 50, "Print R to restart", pg.Color("red"), (150, 325))
+        screen.fill(pg.Color("black"))
+        del apple, snake
+        gameOver.draw(screen)
+        restartHint.draw(screen)
+        pg.display.update()
+        run_inner = True
+        while run_inner:
+            for i in pg.event.get():
+                if i.type == pg.QUIT:
+                    exit()
+                elif i.type == pg.KEYDOWN:
+                    if i.key == pg.K_r:
+                        snake = Snake()
+                        apple = Apple()
+                        score_counter.i = 0
+                        screen.fill(pg.Color('#36454F'))
+                        pg.display.update()
+                        clock.tick(30)
+                        run_inner = False
+                        break
+
+
+    else:
+        snake.is_lose()
+        snake.update(scr=screen)
+        """Updating Apple object & checking out whether snake's head touching it, 
+            then increasing length of the snake and score, delete apple """
+        apple.update(scr=screen, snake_=snake, score=score_counter)
+        if apple.delete is True:
+            del apple
+            apple = Apple()
+
+        # Rendering text for score and putting it on the screen if game is not over
+        Text("Arial", 44, f"Score : {score_counter.i}", pg.Color('gold'),
+             (0, 0), bg_color=pg.Color('black')).draw(screen)
+        # Drawing Borderline
+        borderLine.draw(screen)
+
+        pg.display.update()
+        clock.tick(30)
+
+pg.quit()
---
+++ objects.py
@@ -0,0 +1,109 @@
+import pygame as pg
+from math import pi
+from random import randrange
+
+pg.font.init()
+
+
+class Text:
+    def __init__(self, font_name: str, font_size: int, text: str, fg_color,
+                 pos_text: tuple, smooth=True, bg_color=None):
+        self.font = pg.font.SysFont(font_name, font_size)
+        self.text = text
+        self.pos = pos_text
+        self.text_color = fg_color
+        self.smoothing = smooth
+        self.bg_color = bg_color
+
+    def draw(self, scr):
+        scr.blit(self.font.render(self.text, self.smoothing, self.text_color, self.bg_color),
+                 self.pos)
+
+
+class Line:
+    """Class for creating lines"""
+    def __init__(self, color, coordsArr: list, boldness: int):
+        self.color = color
+        self.start_coords, self.end_coords = coordsArr
+        self.boldness = boldness
+
+    def draw(self, scr):
+        pg.draw.line(scr, self.color,
+                     self.start_coords,
+                     self.end_coords, self.boldness)
+
+
+class Snake:
+    def __init__(self, length=1):
+        self.step = 30
+        self.x = 300
+        self.y = 300
+
+        self.body = [(self.x, self.y)]
+        self.len = length
+        self.speed = 7
+        self.move_x = 0
+        self.move_y = 0
+        self.timer = 0
+
+        self.game_over = False
+
+    def draw(self, scr):
+        [(pg.draw.rect(scr, pg.Color('lightgreen') if (i, j) != self.body[-1] else pg.Color('#39FF14'),
+                       (i, j, self.step - 1, self.step - 1))) for i, j in self.body]
+
+    def move(self):
+        if not (self.timer % self.speed):
+            self.x += self.step * self.move_x
+            self.y += self.step * self.move_y
+            self.body.append((self.x, self.y))
+            self.body = self.body[-self.len:]
+        self.timer += 1
+
+    def is_lose(self):
+        if len(self.body) != len(set(self.body)): self.game_over = True
+        for k in self.body:
+            if any([k[0] > 600, k[0] < 0, k[1] < 50, k[1] > 650]):
+                self.game_over = True
+
+    def update(self, **kwargs):
+        if kwargs.get('scr'):
+            self.move()
+            self.draw(kwargs.get('scr'))
+
+
+class Score:
+    def __init__(self):
+        self.i = 0
+
+    def inc(self):
+        self.i += 10
+
+
+class Apple:
+    def __init__(self):
+        self.size = 25
+        self.x = randrange(55, 550, self.size)
+        self.y = randrange(55, 600, self.size)
+        self.delete = False
+
+    def draw(self, scr):
+        pg.draw.rect(scr, pg.Color('red'), (self.x, self.y, self.size, self.size))
+
+    def is_collide(self, snk, score):
+        if pg.Rect.colliderect(pg.Rect((self.x, self.y, self.size, self.size)),
+                               pg.Rect((snk.body[0][0], snk.body[0][1], snk.step, snk.step))):
+            self.delete = True
+            score.i += 1
+            snk.len += 1
+            snk.speed -= 1
+            snk.speed = max(snk.speed, 3)
+
+    def update(self, **kwargs):
+        if kwargs.get('scr') and kwargs.get('snake_'):
+            self.is_collide(kwargs.get('snake_'), kwargs.get('score'))
+            self.draw(kwargs.get('scr'))
+
+
+if __name__ == "__main__":
+    pass
